8e4a1254c53831a3d7f9ad324a5aa17a02e1562e
Santos Fosler <55976917+wickteam@users.noreply.github.com>
2024-06-19 15:49:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.setcreed.top": dockerHub,
+  "quay.setcreed.top": "https://quay.io",
+  "gcr.setcreed.top": "https://gcr.io",
+  "k8s-gcr.setcreed.top": "https://k8s.gcr.io",
+  "k8s.setcreed.top": "https://registry.k8s.io",
+  "ghcr.setcreed.top": "https://ghcr.io",
+  "cloudsmith.setcreed.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.setcreed.top": dockerHub,
 };
 
 function routeByHosts(host) {
